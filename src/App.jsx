154796e169eb272008cc70e03be7274bed57c94e
b1mbula <danja.rubeghanskij@gmail.com>
2024-09-22 15:30:51 +0200

    hw

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import RestrictedRoute from "./components/RestrictedRoute";
 import PrivateRoute from "./components/PrivatRoute";
 import { useDispatch, useSelector } from "react-redux";
 import { apiRefreshUser } from "./redux/auth/operations";
-import { selectIsRefreshing } from "./redux/auth/selectors";
+import { selectIsLoggedIn, selectIsRefreshing } from "./redux/auth/selectors";
+import { apiGetAllContacts } from "./redux/contacts/operations";
 
 const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
 const RegistrationPage = lazy(() =>
@@ -18,14 +19,21 @@ const Layout = lazy(() => import("./components/Layout/Layout"));
 
 function App() {
   const isRefreshing = useSelector(selectIsRefreshing);
+  const isLoggedIn = useSelector(selectIsLoggedIn);
 
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(apiRefreshUser());
   }, [dispatch]);
 
-  if (isRefreshing) return <p>Pleas,wait</p>;
+  useEffect(() => {
+    if (isLoggedIn) {
+      dispatch(apiGetAllContacts());
+    }
+  }, [isLoggedIn, dispatch]);
 
+  if (isRefreshing) return <p>Please wait...</p>;
   return (
     <div className={css.window}>
       <main>
